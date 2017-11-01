1404b95c70b3f548d118a78fc849b6e29279a4e1
Oliver Morris <officialgamermorris@gmail.com>
2017-11-01 14:46:01 +0000

    Add files via upload

---
+++ tuxexchange.py
@@ -0,0 +1,95 @@
+import hmac
+import hashlib
+import time
+import json
+try:
+  from urllib import urlencode
+  from urlparse import urljoin
+except ImportError:
+  from urllib.parse import urlencode
+  from urllib.parse import urljoin
+
+import requests
+
+PublKey = raw_input('Public Key for Tux Exchange')
+
+PrivKey = raw_input('Private Key For Tux Exchange')
+
+BASE_URL = 'https://tuxexchange.com/api?method={method}'
+
+# Possible methods
+PUBLIC_METHODS = [
+  'getticker',
+  'get24hvolume',
+  'getorders',
+  'gettradehistory',
+  'getcoins']
+
+AUTHENTICATED_METHODS = [
+  'getmybalances',
+  'withdraw',
+  'getmyaddresses',
+  'getmytradehistory',
+  'buy',
+  'sell',
+  'getmyopenorders',
+  'cancelorder']
+
+
+def using_requests(request_url):
+  return  requests.get(
+            request_url
+          ).json()
+
+
+class Tuxexchange(object):
+  def __init__(self, calls_per_second=6, dispatch=using_requests):
+    self.dispatch = dispatch
+    self.call_rate = 1.0/calls_per_second
+    self.last_call = None
+
+
+  def wait(self):
+    if self.last_call is None:
+      self.last_call = time.time()
+    else:
+      now = time.time()
+      passed = now - self.last_call
+      if passed < self.call_rate:
+        #print('sleep')
+        time.sleep(1.0 - passed)
+
+      self.last_call = time.time()
+
+
+  def api_query(self, method, options=None):
+    '''
+    Queries Tux Exchange with given method and options.
+    :param method: Method to query
+    :return      : JSON response from Tux Exchange
+    :rtype       : dict
+    '''
+    if not options:
+      options = {}
+    nonce = str(int(time.time() * 1000))
+
+    request_url = BASE_URL.format(method=method)
+
+    request_url += urlencode(options)
+
+    self.wait()
+
+    return self.dispatch(request_url)
+
+  def getBalances(self):
+      query = {"method":"getmybalances"}
+
+      encoded = urlencode(query)
+
+      signature = hmac.new(PrivKey,encoded,hashlib.sha512).hexdigest()
+
+      Tuxexchangeheader  = {'Sign':signature, 'Key': PublKey}
+
+      tuxgetbalance = requests.post(BASE_URL, data=query, headers=Tuxexchange,timeout=15).json()
+
+      
